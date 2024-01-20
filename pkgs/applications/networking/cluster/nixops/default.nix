d10974976471972862eff63743a959da5f9d3245
hausken <79340822+hauskens@users.noreply.github.com>
2024-01-20 11:49:22 +0100

    Update default.nix

--- pkgs/applications/networking/cluster/nixops/default.nix
+++ pkgs/applications/networking/cluster/nixops/default.nix
@@ -8,5 +8,63 @@ let
   };
 in
 {
+  plugins = ps: with ps; rec {
+    nixops-aws = callPackage ./plugins/nixops-aws.nix { };
+    nixops-digitalocean = callPackage ./plugins/nixops-digitalocean.nix { };
+    nixops-encrypted-links = callPackage ./plugins/nixops-encrypted-links.nix { };
+    nixops-gce = callPackage ./plugins/nixops-gce.nix { };
+    nixops-hercules-ci = callPackage ./plugins/nixops-hercules-ci.nix { };
+    #nixops-hetzner = callPackage ./plugins/nixops-hetzner.nix { };
+    #nixops-hetznercloud = callPackage ./plugins/nixops-hetznercloud.nix { };
+    nixops-libvirtd = callPackage ./plugins/nixops-libvirtd.nix { };
+    nixops-vbox = callPackage ./plugins/nixops-vbox.nix { };
+    nixos-modules-contrib = callPackage ./plugins/nixos-modules-contrib.nix { };
 
+    # aliases for backwards compatibility
+    nixops-gcp = nixops-gce;
+    nixops-virtd = nixops-libvirtd;
+    nixopsvbox = nixops-vbox;
+  };
+
+  # selector is a function mapping pythonPackages to a list of plugins
+  # e.g. nixops_unstable.withPlugins (ps: with ps; [ nixops-aws ])
+  withPlugins = selector: let
+    selected = selector (plugins python.pkgs);
+  in python.pkgs.toPythonApplication (python.pkgs.nixops.overridePythonAttrs (old: {
+    propagatedBuildInputs = old.propagatedBuildInputs ++ selected;
+
+    # Propagating dependencies leaks them through $PYTHONPATH which causes issues
+    # when used in nix-shell.
+    postFixup = ''
+      rm $out/nix-support/propagated-build-inputs
+    '';
+
+    passthru = old.passthru // {
+      plugins = plugins python.pkgs;
+      inherit withPlugins python;
+    };
+  }));
+in lib.mapAttrs
+  (name: plugnames: withPlugins (ps: map (k: ps.${k}) plugnames) )
+  {
+    minimal      = [];
+    aws          = [ "nixops-aws" ];
+    digitalocean = [ "nixops-digitalocean" ];
+    gce          = [ "nixops-gce" ];
+    hercules-ci  = [ "nixops-hercules-ci" ];
+    #hetzner      = [ "nixops-hetzner" "nixops-hetznercloud" ];
+    libvirtd     = [ "nixops-libvirtd" ];
+    vbox         = [ "nixops-vbox" ];
+    full         = [
+      #"nixops-aws"
+      #"nixops-digitalocean"
+      #"nixops-encrypted-links"
+      #"nixops-gce"
+      #"nixops-hercules-ci"
+      #"nixops-hetzner"
+      #"nixops-hetznercloud"
+      #"nixops-libvirtd"
+      "nixops-vbox"
+    ];
+  }
 }
